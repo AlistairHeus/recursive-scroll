8658f6971f20e07bbc023a0b56cbc83eb3554173
Kartik Jha <kartikjha13@gmail.com>
2022-11-04 14:43:27 +0530

    Add files via upload

--- src/script.js
+++ src/script.js
@@ -1,353 +1,21 @@
-// import './style.css'
-
-// import * as THREE from 'three'
-
-// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-
-// import fragment from './shader/fragment.glsl'
-// import fragment1 from './shader/fragment1.glsl'
-// import vertex from './shader/vertex.glsl'
-
-// // three/examples/jsm/postprocessing/EffectComposer
-
-// import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js'
-// import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js'
-// import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js'
-// import { PostProcessing } from './postprocessing'
-
-// import gsap from 'gsap'
-// import * as dat from 'dat.gui'
-
-// /**
-//  * Base
-//  */
-// const parameters = {}
-// parameters.radius = 1
-// parameters.detail = 1
-// parameters.wireframe = false
-// parameters.mouse = true
-// parameters.images = 'dune-du-pilat'
-// parameters.howmuchrgbshifticanhaz = 1
-
-// // Canvas
-// const canvas = document.querySelector('canvas.webgl')
-
-// // Scene
-// const scene = new THREE.Scene()
-
-// /**
-//  * Object
-//  */
-// let geometry = null
-// let geometry1 = null
-// let material = null
-// let material1 = null
-// let icosahedron = null
-// let icosahedronLines = null
-
-// const generateIcosahedron = () => {
-//   if (icosahedron !== null) {
-//     geometry.dispose()
-//     material.dispose()
-
-//     geometry1.dispose()
-//     material1.dispose()
-
-//     scene.remove(icosahedron)
-//     scene.remove(icosahedronLines)
-//   }
-
-//   /**
-//    * GEOMETRIES
-//    */
-//   geometry = new THREE.IcosahedronGeometry(parameters.radius, parameters.detail)
-
-//   geometry1 = new THREE.IcosahedronBufferGeometry(
-//     parameters.radius,
-//     parameters.detail
-//   ) //(1.001, 1)
-
-//   let length = geometry1.attributes.position.array.length
-//   let bary = []
-
-//   for (let i = 0; i < length / 3; i++) {
-//     // xyz, xyz, xyz
-//     bary.push(0, 0, 1, 0, 1, 0, 1, 0, 0)
-//   }
-
-//   let aBary = new Float32Array(bary)
-//   geometry1.setAttribute('aBary', new THREE.BufferAttribute(aBary, 3))
-
-//   let t = new THREE.TextureLoader().load(`images/${parameters.images}.jpg`)
-//   t.wrapS = t.wrapT = THREE.MirroredRepeatWrapping
-
-//   /**
-//    * MATERIALS
-//    */
-//   material = new THREE.ShaderMaterial({
-//     extensions: {
-//       derivatives: '#extension GL_OES_standard_derivatives : enable',
-//     },
-//     side: THREE.DoubleSide,
-//     uniforms: {
-//       time: { type: 'f', value: 0 },
-//       mouse: { value: 0 },
-//       landscape: {
-//         value: t,
-//       },
-//       resolution: { type: 'v4', value: new THREE.Vector4() },
-//       uvRate1: {
-//         value: new THREE.Vector2(1, 1),
-//       },
-//     },
-//     wireframe: parameters.wireframe,
-//     // transparent: true,
-//     vertexShader: vertex,
-//     fragmentShader: fragment,
-//   })
-
-//   material1 = new THREE.ShaderMaterial({
-//     extensions: {
-//       derivatives: '#extension GL_OES_standard_derivatives : enable',
-//     },
-//     side: THREE.DoubleSide,
-//     uniforms: {
-//       time: { type: 'f', value: 0 },
-//       mouse: { value: 0 },
-//       landscape: {
-//         value: t,
-//       },
-//       resolution: { type: 'v4', value: new THREE.Vector4() },
-//       uvRate1: {
-//         value: new THREE.Vector2(1, 1),
-//       },
-//     },
-//     wireframe: parameters.wireframe,
-//     // transparent: true,
-//     vertexShader: vertex,
-//     fragmentShader: fragment1,
-//   })
-
-//   /**
-//    * MESHES
-//    */
-//   icosahedron = new THREE.Mesh(geometry1, material)
-//   icosahedronLines = new THREE.Mesh(geometry1, material1)
-
-//   //   scene.add(icosahedron, icosahedronLines)
-//   scene.add(icosahedron)
-//   scene.add(icosahedronLines)
-// }
-
-// generateIcosahedron()
-
-// /**
-//  * Sizes
-//  */
-// const sizes = {
-//   width: window.innerWidth,
-//   height: window.innerHeight,
-// }
-
-// // Handle window resize
-// window.addEventListener('resize', () => {
-//   // update sizes
-//   sizes.width = window.innerWidth
-//   sizes.height = window.innerHeight
-
-//   // update camera
-//   camera.aspect = sizes.width / sizes.height
-//   camera.updateProjectionMatrix()
-
-//   // update renderer
-//   renderer.setSize(sizes.width, sizes.height)
-//   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// })
-
-// window.addEventListener('dblclick', () => {
-//   const fullscreenElement =
-//     document.fullscreenElement || document.webkitFullscreenElement
-
-//   if (!fullscreenElement) {
-//     if (canvas.requestFullscreen) {
-//       canvas.requestFullscreen()
-//     } else if (canvas.webkitRequestFullscreen) {
-//       canvas.webkitRequestFullscreen()
-//     }
-//   } else {
-//     if (document.exitFullscreen) {
-//       document.exitFullscreen()
-//     } else if (document.webkitExitFullscreen) {
-//       document.webkitExitFullscreen()
-//     }
-//   }
-// })
-
-// let mouse = 0
-// let lastX = 0
-// let lastY = 0
-// let speed = 0
-// window.addEventListener('mousemove', (e) => {
-//   //   speed = (e.pageX - lastX) * 0.1
-//   speed = Math.sqrt((e.pageX - lastX) ** 2 + (e.pageY - lastY) ** 2) * 0.1
-
-//   lastX = e.pageX
-//   lastY = e.pageY
-// })
-
-// /**
-//  * Camera
-//  */
-// // Base camera
-// const camera = new THREE.PerspectiveCamera(
-//   70,
-//   sizes.width / sizes.height,
-//   0.001,
-//   1000
-// )
-
-// camera.position.x = 0
-// camera.position.y = 0
-// camera.position.z = 2
-
-// scene.add(camera)
-
-// // Controls
-// const controls = new OrbitControls(camera, canvas)
-// controls.enableDamping = true
-
-// /**
-//  * Renderer
-//  */
-// const renderer = new THREE.WebGLRenderer({
-//   canvas: canvas,
-//   alpha: true,
-// })
-
-// renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-// renderer.setSize(sizes.width, sizes.height)
-// // renderer.setClearColor(0x111111, 1)
-// renderer.outputEncoding = THREE.sRGBEncoding
-
-// /**
-//  * Effect Composer
-//  */
-// let composer = new EffectComposer(renderer)
-// composer.setSize(sizes.width, sizes.height)
-// composer.addPass(new RenderPass(scene, camera))
-
-// let customPass = new ShaderPass(PostProcessing)
-// customPass.uniforms['resolution'].value = new THREE.Vector2(
-//   window.innerWidth,
-//   window.innerHeight
-// )
-// customPass.uniforms['resolution'].value.multiplyScalar(window.devicePixelRatio)
-// composer.addPass(customPass)
-
-// /**
-//  * Debug
-//  */
-// const gui = new dat.GUI({
-//   //   closed: true,
-//   width: 250,
-// })
-// // gui.hide()
-
-// gui
-//   .add(parameters, 'radius')
-//   .min(1)
-//   .max(20)
-//   .step(1)
-//   .onFinishChange(generateIcosahedron)
-// gui
-//   .add(parameters, 'detail')
-//   .min(1)
-//   .max(20)
-//   .step(1)
-//   .onFinishChange(generateIcosahedron)
-// gui.add(parameters, 'wireframe').onChange(generateIcosahedron)
-
-// gui
-//   .add(parameters, 'mouse')
-//   .onChange((val) => {
-//     console.log({ val, mouse: parameters.mouse })
-//   })
-//   .name('mouse tracking')
-
-// gui
-//   .add(parameters, 'images', [
-//     'dune-du-pilat',
-//     'pilat-beach',
-//     'pilat-sand',
-//     'bordeaux',
-//     'sand',
-//     'okinawa-sea',
-//     'sakura',
-//     'geisha',
-//   ])
-//   .onFinishChange(generateIcosahedron)
-
-// // gui
-// //   .add(parameters, 'howmuchrgbshifticanhaz')
-// //   .min(0)
-// //   .max(1)
-// //   .step(0.01)
-// //   .name('RGB shift')
-
-// /**
-//  * Animate
-//  */
-// const clock = new THREE.Clock()
-
-// const tick = () => {
-//   const isMouseEnabled = parameters.mouse
-//   const elapsedTime = clock.getElapsedTime()
-
-//   mouse -= (mouse - speed) * 0.05
-//   // mouse *= 0.99
-//   speed *= 0.99
-
-//   icosahedron.rotation.x = elapsedTime * 0.04
-//   icosahedron.rotation.y = elapsedTime * 0.04
-
-//   icosahedronLines.rotation.x = elapsedTime * 0.04
-//   icosahedronLines.rotation.y = elapsedTime * 0.04
-
-//   customPass.uniforms.time.value = elapsedTime
-//   customPass.uniforms.howmuchrgbshifticanhaz.value = isMouseEnabled
-//     ? mouse / 5
-//     : 0.3 //parameters.howmuchrgbshifticanhaz
-
-//   material.uniforms.time.value = elapsedTime
-//   material1.uniforms.time.value = elapsedTime
-
-//   if (isMouseEnabled) {
-//     material.uniforms.mouse.value = mouse
-//     material1.uniforms.mouse.value = mouse
-//   } else {
-//     material.uniforms.mouse.value = 0
-//     material1.uniforms.mouse.value = 0
-//   }
-
-//   // Update controls
-//   controls.update()
-
-//   // Render
-//   // renderer.render(scene, camera)
-//   composer.render()
-
-//   // Call tick again on the next frame
-//   window.requestAnimationFrame(tick)
-// }
-
-// tick()
-
 import * as THREE from "three";
 import fragment from "./shaders/fragment.glsl";
 import vertex from "./shaders/vertex.glsl";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 // init
+
+import img1 from "../static/images/bordeaux.jpg";
+import img2 from "../static/images/dune-du-pilat.jpg";
+import img3 from "../static/images/geisha.jpg";
+import img4 from "../static/images/okinawa-sea.jpg";
+import img5 from "../static/images/pilat-beach.jpg";
+import img6 from "../static/images/pilat-sand.jpg";
+import disp from "../static/images/disp.png";
+
+const images = [img1, img2, img3, img4, img5, img6];
+
+const textures = images.map((img) => new THREE.TextureLoader().load(img));
 
 export default class Sketch {
   constructor(options) {
@@ -356,50 +24,75 @@ export default class Sketch {
     this.container = options.dom;
     this.width = this.container.offsetWidth;
     this.height = this.container.offsetHeight;
-    this.renderer = new THREE.WebGLRenderer();
+    this.renderer = new THREE.WebGLRenderer({
+      alpha: true,
+      antialias: true,
+    });
     this.renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-    this.renderer.setSize(this.width, this.height);
-    this.renderer.setClearColor(0xeeeeee, 1);
-    this.renderer.physicallyCorrectLights = true;
-    this.renderer.outputEncoding = THREE.sRGBEncoding;
     this.scroll = 0;
     this.scrollTarget = 0;
     this.currentScroll = 0;
+    this.renderer.setSize(this.width, this.height);
+    // this.renderer.setClearColor(0x000000, 1);
+    this.renderer.physicallyCorrectLights = true;
+    this.renderer.outputEncoding = THREE.sRGBEncoding;
+
     this.container.appendChild(this.renderer.domElement);
 
-    this.camera = new THREE.PerspectiveCamera(
-      70,
-      window.innerWidth / window.innerHeight,
-      0.001,
+    // this.camera = new THREE.PerspectiveCamera(
+    //   70,
+    //   window.innerWidth / window.innerHeight,
+    //   0.001,
+    //   1000
+    // );
+
+    this.renderTarget = new THREE.WebGLRenderTarget(this.width, this.height, {
+      format: THREE.RGBAFormat,
+      magFilter: THREE.NearestFilter,
+      minFilter: THREE.NearestFilter,
+    });
+
+    this.renderTarget1 = new THREE.WebGLRenderTarget(this.width, this.height, {
+      format: THREE.RGBAFormat,
+      magFilter: THREE.NearestFilter,
+      minFilter: THREE.NearestFilter,
+    });
+
+    let frustumSize = 4;
+
+    let aspect = window.innerWidth / window.innerHeight;
+    this.aspect = this.width / this.height;
+
+    this.camera = new THREE.OrthographicCamera(
+      (frustumSize * aspect) / -2,
+      (frustumSize * aspect) / 2,
+      frustumSize / 2,
+      frustumSize / -2,
+      -1000,
       1000
     );
 
     this.camera.position.set(0, 0, 2);
-    // this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+
     this.time = 0;
+
+    this.backgroundQuad = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(4 * this.aspect, 4),
+      new THREE.MeshBasicMaterial({
+        // transparent: true,
+      })
+    );
+    // this.backgroundQuad.position.y = 0.5;
+    this.backgroundQuad.position.z = -0.5;
+    this.scene.add(this.backgroundQuad);
+
     this.isPlaying = true;
+    this.initQuad();
     this.addObjects();
     this.resize();
     this.render();
     this.setupResize();
     this.scrollEvent();
-    //  this.container = options.dom;
-
-    //   this.renderer = new THREE.WebGLRenderer({ antialias: true });
-    //   this.renderer.setSize(window.innerWidth, window.innerHeight);
-    //   document.getElementById("container").appendChild(this.renderer.domElement);
-
-    //   this.camera = new THREE.PerspectiveCamera(
-    //     70,
-    //     window.innerWidth / window.innerHeight,
-    //     0.01,
-    //     10
-    //   );
-    //   this.camera.position.z = 1;
-    //   this.scene = new THREE.Scene();
-    //   this.addMesh();
-    //   this.time = 0;
-    //   this.render();
   }
 
   scrollEvent() {
@@ -407,6 +100,46 @@ export default class Sketch {
       this.scrollTarget = e.wheelDelta * 0.3;
     });
   }
+
+  initQuad() {
+    this.sceneQuad = new THREE.Scene();
+
+    this.materialQuad = new THREE.ShaderMaterial({
+      extensions: {
+        derivatives: "#extension GL_OES_standard_derivatives : enable",
+      },
+      side: THREE.DoubleSide,
+      uniforms: {
+        time: { value: 0 },
+        uTexture: { value: null },
+        uDisp: { value: new THREE.TextureLoader().load(disp) },
+        speed: { value: 0 },
+        dir: { value: 0 },
+        resolution: { value: new THREE.Vector4() },
+      },
+      transparent: true,
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+
+    // this.materialQuad = new THREE.MeshBasicMaterial();
+
+    this.quad = new THREE.Mesh(
+      new THREE.PlaneBufferGeometry(4 * this.aspect, 4),
+      this.materialQuad
+    );
+
+    this.sceneQuad.add(this.quad);
+  }
+
+  // settings() {
+  //   let that = this;
+  //   this.settings = {
+  //     progress: 0,
+  //   }
+  //   this.gui = new GUI();
+  //   this.gui.add(this.settings, "progress", 0, 1, 0.01)
+  // }
 
   setupResize() {
     window.addEventListener("resize", this.resize.bind(this));
@@ -428,84 +161,112 @@ export default class Sketch {
       a1 = 1;
       a2 = this.height / this.width / this.imageAspect;
     }
-
-    this.material.uniforms.resolutions.value.x = this.width;
-    this.material.uniforms.resolutions.value.y = this.height;
-    this.material.uniforms.resolutions.value.z = a1;
-    this.material.uniforms.resolutions.value.w = a2;
-
     this.camera.updateProjectionMatrix();
   }
 
   addObjects() {
-    // this.geometry = new THREE.PlaneBufferGeometry(1, 1);
-    // this.material = new THREE.MeshNormalMaterial({ side: THREE.DoubleSide });
-
-    this.material = new THREE.ShaderMaterial({
-      vertexShader: vertex,
-      fragmentShader: fragment,
-      uniforms: {
-        // progress: { type: "f", value: 0 },
-
-        time: { value: 0 },
-        resolutions: { value: new THREE.Vector4() },
-      },
-      side: THREE.DoubleSide,
-    });
+    // let that = this;
 
     this.geometry = new THREE.PlaneGeometry(1, 1, 1, 1);
 
     this.meshes = [];
+    this.n = 10;
 
-    for (let i = 0; i < 10; i++) {
-      let mesh = new THREE.Mesh(this.geometry, this.material);
-
+    for (let i = 0; i < this.n; i++) {
+      let mesh = new THREE.Mesh(
+        this.geometry,
+        new THREE.MeshBasicMaterial({
+          map: textures[i % textures.length],
+        })
+      );
       this.meshes.push({
         mesh,
         index: i,
       });
       this.scene.add(mesh);
     }
-
-    this.plane = new THREE.Mesh(this.geometry, this.material);
-
-    this.scene.add(this.plane);
-
-    // this.mesh = new THREE.Points(this.geometry, this.material);
-    // this.scene.add(this.mesh);
   }
 
-  stop() {
-    this.isPlaying = false;
-  }
-  play() {
-    if (!this.isPlaying) {
-      (this.isPlaying = true), this.render();
-    }
-  }
+  // stop() {
+  //   this.isPlaying = false;
+  // }
+  // play() {
+  //   if (!this.isPlaying) {
+  //     (this.isPlaying = true), this.render();
+  //   }
+  // }
 
   updateMeshes() {
+    this.margin = 1.1;
+    this.wholeWidth = this.n * this.margin;
     this.meshes.forEach((o) => {
-      o.mesh.position.x = 1.1 * o.index + this.currentScroll;
+      o.mesh.position.x =
+        ((this.margin * o.index +
+          this.currentScroll +
+          42069 * this.wholeWidth) %
+          this.wholeWidth) -
+        this.margin * 2;
     });
   }
+
+  // render() {
+  //   if (!this.isPlaying) return;
+  //   this.time += 0.05;
+  //   this.scroll += (this.scrollTarget - this.scroll) * 0.1;
+  //   this.scroll *= 0.9;
+  //   this.scrollTarget *= 0.9;
+  //   this.currentScroll += this.scroll * 0.01;
+  //   this.updateMeshes();
+  //   window.requestAnimationFrame(this.render.bind(this));
+
+  //   // default texture
+  //   this.renderer.setRenderTarget(this.renderTarget);
+  //   this.renderer.render(this.scene, this.camera);
+
+  //   // render distorted texture
+
+  //   this.renderer.setRenderTarget(this.renderTarget1);
+  //   this.materialQuad.uniforms.uTexture.value = this.renderTarget.texture;
+  //   this.renderer.render(this.sceneQuad, this.camera);
+
+  //   // // final scene
+  //   this.renderer.setRenderTarget(null);
+  //   this.backgroundQuad.material.map = this.renderTarget1.texture;
+  //   this.renderer.render(this.scene, this.camera);
+  // }
 
   render() {
     if (!this.isPlaying) return;
 
     this.time += 0.05;
     this.scroll += (this.scrollTarget - this.scroll) * 0.1;
-    this.scroll += 0.9;
-    this.scrollTarget += 0.9;
-    console.log(this.scroll);
-
+    this.scroll *= 0.9;
+    this.scrollTarget *= 0.9;
     this.currentScroll += this.scroll * 0.01;
-
     this.updateMeshes();
     // this.mesh.rotation.y += 0.02;
-    this.material.uniforms.time.value = this.time;
+
+    requestAnimationFrame(this.render.bind(this));
+
+    //default texture
+    this.renderer.setRenderTarget(this.renderTarget);
     this.renderer.render(this.scene, this.camera);
-    window.requestAnimationFrame(this.render.bind(this));
+
+    //render distorted texture
+    this.renderer.setRenderTarget(this.renderTarget1);
+    this.materialQuad.uniforms.uTexture.value = this.renderTarget.texture;
+
+    this.materialQuad.uniforms.speed.value = Math.min(
+      0.3,
+      Math.abs(this.scroll)
+    );
+    this.materialQuad.uniforms.dir.value = Math.sign(this.scroll);
+    this.renderer.render(this.sceneQuad, this.camera);
+
+    // render final scene
+    this.renderer.setRenderTarget(null);
+    this.backgroundQuad.material.map = this.renderTarget1.texture;
+    this.renderer.render(this.scene, this.camera);
   }
 }
 
--- src/shaders/fragment.glsl
+++ src/shaders/fragment.glsl
@@ -1,7 +1,47 @@
-void main() {
-    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
+// uniform float time;
+// uniform float progress;
+// uniform float speed;
+// uniform float dir;
+// uniform sampler2D uTexture;
+// uniform vec4 resolution;
+// varying vec2 vUv;
+// varying vec3 vPosition;
+// float PI = 3.14159265;
 
-    
 
+// void main(){
+
+//   // gl_FragColor = vec4(vUv,1.0,1.);
+
+//   float force = pow(length(vUv.x) + 0.5, abs(speed*0.01));
+//   vec2 newUV = vUv* cos(1.-force)
+
+//    gl_FragColor = 0.8*texture2D(uTexture,vUv+ vec2(0., -0.03));
+// }
+
+uniform float time;
+uniform float progress;
+uniform float speed;
+uniform float dir;
+uniform sampler2D uTexture;
+uniform sampler2D uDisp;
+uniform vec4 resolution;
+varying vec2 vUv;
+varying vec3 vPosition;
+
+
+
+float PI = 3.14159265;
+void main(){
+ vec4 d =texture2D(
+    uDisp,
+    (vUv - vec2(0.5))*(1. + speed ) + vec2(0.5)
+    );
+    float force = pow(length(vUv.x) + 0.5, abs(speed));
+    vec2 newuv = vUv*cos(1.-force);
+
+    gl_FragColor = (0.1 + 2.*speed)*texture2D(uTexture,newuv + d.xy*0.2 + vec2(0.,-0.03));
+   
+   
+    gl_FragColor = 0.8*texture2D(uTexture,newuv + d.xy*0.02 + vec2(0.,-0.03));
 }
-
